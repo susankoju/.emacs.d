2428d70322872cd9dcaff5d7da4d47c499b63cd4
susankoju <aksusankoju@gmail.com>
2020-12-27 18:24:19 +0545

    Add slime

--- init.el
+++ init.el
@@ -1,6 +1,6 @@
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+
 (package-initialize)
 
 (defvar my-packages '(better-defaults
@@ -11,3 +11,18 @@
 (dolist (p my-packages)
   (unless (package-installed-p p)
     (package-install p)))
+
+(load (expand-file-name "~/.quicklisp/slime-helper.el"))
+(setq inferior-lisp-program "/usr/bin/sbcl")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (hy-mode projectile cider better-defaults))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
